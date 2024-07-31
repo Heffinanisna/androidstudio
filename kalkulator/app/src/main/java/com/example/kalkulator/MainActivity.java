861e73bc1b84650c8a278e37638e57ec78c85b6c
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-07-31 10:43:36 +0700

    vid 5

--- kalkulator/app/src/main/java/com/example/kalkulator/MainActivity.java
+++ kalkulator/app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.kalkulator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvhasil;
+    EditText etbil_1, etbil_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        load();
+    }
+
+    public void load (){
+        tvhasil = findViewById(R.id.tvhasil);
+        etbil_1 = findViewById(R.id.etbil_1);
+        etbil_2 = findViewById(R.id.etbil_2);
+    }
+
+    public void btnjumlah(View view) {
+
+        if (etbil_1.getText().toString().equals("") || etbil_2.getText().toString().equals("")) {
+            Toast.makeText(this, "ada bilangan kosong", Toast.LENGTH_SHORT).show();
+        } else {
+
+            Double bil_1 = Double.parseDouble(etbil_1.getText().toString());
+            Double bil_2 = Double.parseDouble(etbil_2.getText().toString());
+
+            Double hasil = bil_1 + bil_2;
+            tvhasil.setText(hasil + "");
+        }
     }
 }
