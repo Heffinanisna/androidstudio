b64d972b62a6674e5dc941a7b260fe9d37958b13
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-08-01 07:41:17 +0700

    vid 7

--- KonversSuhu/app/src/main/java/com/example/konverssuhu/MainActivity.java
+++ KonversSuhu/app/src/main/java/com/example/konverssuhu/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.konverssuhu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        load();
+        isiSpinner();
+    }
+    public void load(){
+        spinner = findViewById(R.id.spinner);
+    }
+
+    public void isiSpinner(){
+        String[] isi = {"Celcius to Reamur","Celius to Fahreinheit","Celcius to Kelvin"};
+        ArrayAdapter<String>adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
+        spinner.setAdapter(adapter);
+    }
+
+
+    public void btnkonversi(View view) {
+        String pilihan = spinner.getSelectedItem().toString();
+
+        System.out.println(pilihan);
     }
 }
