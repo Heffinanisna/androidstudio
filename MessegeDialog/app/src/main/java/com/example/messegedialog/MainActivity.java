a40e115c1b9eb5d83a2e744eab05a0e44ed69ee5
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-08-01 10:12:49 +0700

    vid 11

--- MessegeDialog/app/src/main/java/com/example/messegedialog/MainActivity.java
+++ MessegeDialog/app/src/main/java/com/example/messegedialog/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.messegedialog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -21,41 +25,49 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        System.out.println("onCreate");
+    }
+    public void showToast (String pesan){
+        Toast.makeText(this,"pesan",Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    public void showAlert (String pesan){
+        AlertDialog.Builder buatAlert = new AlertDialog.Builder(this);
+        buatAlert.setTitle("PERHATIAN!");
+        buatAlert.setMessage(pesan);
 
-        System.out.println("onStart");
+        buatAlert.show();
     }
 
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
+    public void showalertbutton (String pesan){
+        AlertDialog.Builder showAlert = new AlertDialog.Builder(this);
+        showAlert.setTitle("PERINGATAN !");
+        showAlert.setMessage(pesan);
 
-        System.out.println("onResume");
+        showAlert.setPositiveButton("IYA", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                showToast("Data di hapus");
+            }
+        });
+        showAlert.setNegativeButton("TIDAK", new DialogInterface.OnClickListener(){
+            @Override
+                    public void onClick(DialogInterface dialog, int which){
+                        showAlert("data tidak dihapus !");
+            }
+        });
+
+        showAlert.show();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        System.out.println("onPause");
+    public void btntoast(View view) {
+        showToast("belajar belajar");
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        System.out.println("onStop");
+    public void btnalert(View view) {
+        showToast("selamat belajar !");
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        System.out.println("onDestroy");
+    public void btndialogbutton(View view) {
+        showalertbutton("yakin akan menghapus ?");
     }
 }
