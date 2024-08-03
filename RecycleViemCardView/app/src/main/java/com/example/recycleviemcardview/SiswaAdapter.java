ec680d5421f17956839d626777160fed74cc7a8b
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-08-03 17:58:47 +0700

    vid 21

--- RecycleViemCardView/app/src/main/java/com/example/recycleviemcardview/SiswaAdapter.java
+++ RecycleViemCardView/app/src/main/java/com/example/recycleviemcardview/SiswaAdapter.java
@@ -2,15 +2,16 @@ package com.example.recycleviemcardview;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -37,12 +38,41 @@ public class SiswaAdapter extends RecyclerView.Adapter<SiswaAdapter.ViewHolder> 
         ViewHolder.tvnama.setText(siswa.getnama());
         ViewHolder.tvalamat.setText(siswa.getAlamat());
 
+        /*
+
         ViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "nama : "+siswa.getnama()+"alamat: "+siswa.getAlamat(), Toast.LENGTH_SHORT).show();
             }
         });
+        */
+         ViewHolder.tvMenu.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 PopupMenu popupMenu = new PopupMenu(context,ViewHolder.tvMenu);
+                 popupMenu.inflate (R.menu.menu_option);
+
+                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                     @Override
+                     public boolean onMenuItemClick(MenuItem item) {
+
+                        int id = item.getItemId();
+
+                                if (id == R.id.menu_simpan){
+                                    Toast.makeText(context, "Simpan Data"+siswa.getnama(), Toast.LENGTH_SHORT).show();
+                                }else if (id == R.id.menu_hapus){
+                                    siswaList.remove(ViewHolder.getAdapterPosition());
+                                    notifyDataSetChanged();
+                                    Toast.makeText(context, siswa.getnama()+"Sudah Dihapus", Toast.LENGTH_SHORT).show();
+                                }
+                         return false;
+                     }
+                 });
+
+                 popupMenu.show();
+             }
+         });
     }
 
     @Override
@@ -52,13 +82,14 @@ public class SiswaAdapter extends RecyclerView.Adapter<SiswaAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView tvnama, tvalamat;
+        TextView tvnama, tvalamat, tvMenu;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             tvnama = itemView.findViewById(R.id.tvnama);
             tvalamat = itemView.findViewById(R.id.tvalamat);
+            tvMenu = itemView.findViewById(R.id.tvMenu);
         }
     }
 }
