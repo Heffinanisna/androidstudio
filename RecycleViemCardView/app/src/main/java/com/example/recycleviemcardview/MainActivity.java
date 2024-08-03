51bbb268086b34e30436fbbce1f6979dfacfe9f6
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-08-03 15:16:38 +0700

    vid 19

--- RecycleViemCardView/app/src/main/java/com/example/recycleviemcardview/MainActivity.java
+++ RecycleViemCardView/app/src/main/java/com/example/recycleviemcardview/MainActivity.java
@@ -7,8 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SiswaAdapter adapter;
+    List<Siswa>siswaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        load();
+        isidata();
+    }
+    public void load (){
+        recyclerView = findViewById(R.id.rcvsiswa);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    public void isidata(){
+        siswaList = new ArrayList<Siswa>();
+        siswaList.add(new Siswa("Jaemin", "Surabaya"));
+        siswaList.add(new Siswa("Jeno", "Bandung"));
+        siswaList.add(new Siswa("Mark", "Jakarta"));
+        siswaList.add(new Siswa("Renjun", "Jogjakarta"));
+        siswaList.add(new Siswa("Jisung", "Malang"));
+        siswaList.add(new Siswa("Haechan", "Bandung"));
+        siswaList.add(new Siswa("Chenle", "Bogor"));
+
+        adapter = new SiswaAdapter(this,siswaList);
+        recyclerView.setAdapter(adapter);
+
     }
 }
